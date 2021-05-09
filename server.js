3a9309c06c71071a66d4c8ce5e337b3bff5f708f
ShambhuJha <shambhu.jha014@gmail.com>
2021-05-09 13:35:25 +0530

    Added Gmail smpt config

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+require("dotenv").config();
+
+let email = process.env.EMAIL;
+let password = process.env.PASSWORD;
+
 // create express app
 const app = express();
 
@@ -10,11 +16,41 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // define a simple route
-app.get('/test', (req, res) => {
+app.get('/notifyuser', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    // sendemail();
 });
 
 // listen for requests
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
+
+function sendemail(){
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+  });
+  
+  var mailOptions = {
+    from: 'info.mailer014@gmail.com',
+    to: 'shambhu.jha014@gmail.com',
+    subject: 'Covid 19 VACCINATION SLOT NOW AVAILABLE',
+    text: 'That was easy!'
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
