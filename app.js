2cc9e696adebaa09d83518bcfe844adf3a864c61
ShambhuJha <shambhu.jha014@gmail.com>
2021-05-09 12:51:31 +0530

    server setup

---
+++ app.js
@@ -0,0 +1,29 @@
+// var nodemailer = require('nodemailer');
+// require("dotenv").config();
+
+// let email = process.env.EMAIL;
+// let password = process.env.PASSWORD;
+
+
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: email,
+//     pass: password
+//   }
+// });
+
+// var mailOptions = {
+//   from: '####youremail#####',
+//   to: '###targetemail#####',
+//   subject: 'Sending Email using Node.js',
+//   text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
